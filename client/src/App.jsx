6737be5b95d2097ed7c1c8e36f415e90c4df84df
Firdaous14 <fridajmatt@gmail.com>
2024-01-03 11:21:08 +0000

    users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+import Users from './Users'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +14,9 @@ function App() {
       <div>
         <BrowserRouter>
           <Routes>
-          <Route path='/' element={}></Route>
-          <Route path='/' element={}></Route>
-          <Route path='/' element={}></Route>
+          <Route path='/' element={<Users />}></Route>
+          <Route path='/' element={<CreateUser />}></Route>
+          <Route path='/' element={<UpdateUser />}></Route>
           </Routes>
         </BrowserRouter>
       </div>
---
+++ client/src/Users.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+function Users() {
+    const [users, setUsers] = useState([{
+        Name: "Firdaous", Email: "fridajmatt@gamil.com", Age: 20 
+    }])
+  return (
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+        <div className='w-50 bg-white rounded p-3'>
+            <table className='table'>
+                <thead>
+                    <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Age</th>
+                    <th>Action</th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map((user) => {
+                            return <tr>
+                                <td>{user.Name}</td>
+                                <td>{user.Email}</td>
+                                <td>{user.Age}</td>
+                                <td><button>Edit</button><button>Delete</button></td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    </div>
+  )
+}
+
+export default Users
